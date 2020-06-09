63554d6c316c7663add7083337b1fc6dfb59b7ca
178891440@qq.com <178891440@qq.com>
2020-06-09 23:34:29 +0800

    动态地图定位

--- src/views/mydynamic/PublishDynamic.vue
+++ src/views/mydynamic/PublishDynamic.vue
@@ -135,8 +135,8 @@
                 // 接收位置信息，用户选择确认位置点后选点组件会触发该事件，回传用户的位置信息
                 let loc = event.data;
                 if (loc && loc.module == 'locationPicker') {//防止其他应用也会向该页面post信息，需判断module是否为'locationPicker'
-                    console.log('location', loc);
-                    alert(JSON.stringify(loc));
+                    // console.log('location', loc);
+                    // alert(JSON.stringify(loc));
                     this.location = loc.poiaddress;
                     this.showLocationPicker = false;
                 }
@@ -144,7 +144,7 @@
             },
             async initData(){
                 let result = await getTopicList();
-                console.log("result:",result);
+                // console.log("result:",result);
                 if (result.success){
                     this.subjectList = result.data.map(data => data.topic);
                 }
